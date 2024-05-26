924d4dd3b6bd091ebcd2e50b2ab6eb6c520e5e65
ved6053 <ved.cse@gmail.com>
2024-05-26 22:12:42 +0530

    03-EXERCISE 3 // Check that each entry within expected +/- delta -> isAbsDifferenceLessOrEqualToDelta

--- src/main/java/com/refacoring/workbook/matcher/Matcher.java
+++ src/main/java/com/refacoring/workbook/matcher/Matcher.java
@@ -8,21 +8,24 @@ public class Matcher {
 
         makeLargeUpToClipLimit(actual, clipLimit);
 
-        // Check for length differences
-        if (actual.length != expected.length)
-            return false;
 
-        // Check that each entry within expected +/- delta
-        for (int i = 0; i < actual.length; i++)
-            if (Math.abs(expected[i] - actual[i]) > delta)
-                return false;
-
-        return true;
+        return isLengthOfActualEqualToExpected(actual.length , expected.length)?isAbsDifferenceLessOrEqualToDelta(actual, expected, delta):false;
     }
 
     private void makeLargeUpToClipLimit(int []actual, int clipLimit){
         for (int i = 0; i < actual.length; i++)
             if (actual[i] > clipLimit)
                 actual[i] = clipLimit;
+    }
+    private boolean isLengthOfActualEqualToExpected(int actualLength, int expectedLength) {
+        return actualLength==expectedLength;
+    }
+
+    private boolean isAbsDifferenceLessOrEqualToDelta(int[] actual, int[] expected, int delta) {
+        for (int i = 0; i < actual.length; i++)
+            if (Math.abs(expected[i] - actual[i]) > delta)
+                return false;
+
+        return true;
     }
 }
